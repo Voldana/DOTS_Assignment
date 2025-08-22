986fd018a2bb75cae00e9899844ea016ddd6c6f4
Mohammad Darandeh <m.darande@gmail.com>
2025-08-22 14:17:31 -0400

    feat: add movement logic

---
+++ Assets/Project/Scripts/Authoring/BrickAuthoring.cs
@@ -0,0 +1,76 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Project.Scripts.Authoring
+{
+    public class BrickAuthoring : MonoBehaviour
+    {
+        [SerializeField] private BrickKind kind = BrickKind.Normal;
+        [SerializeField] private BrickPath path = BrickPath.None;
+        [SerializeField] private float despawnDelayAfterHit = 0.6f;
+        [SerializeField] private float amplitude = 2f;
+        [SerializeField] private float speed = 0.7f;
+        [SerializeField] private float score = 10f;
+        [SerializeField] private float phase;
+        [SerializeField] private int extraBalls;
+
+        class Baker : Baker<BrickAuthoring>
+        {
+            public override void Bake(BrickAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                var score = authoring.kind == BrickKind.Gold ? authoring.score * 10f : authoring.score;
+                AddComponent(entity,
+                    new Brick
+                    {
+                        kind = authoring.kind, path = authoring.path,
+                        despawnDelayAfterHit = authoring.despawnDelayAfterHit, score = score,
+                        extraBalls = authoring.extraBalls
+                    });
+
+                if (authoring.path == BrickPath.None) return;
+                var pos = authoring.transform.position;
+                var origin = new float2(pos.x, pos.z);
+                AddComponent(entity,
+                    new PathMotion
+                    {
+                        originXY = origin, amplitude = authoring.amplitude, speed = authoring.speed,
+                        phase = authoring.phase
+                    });
+            }
+        }
+
+        public struct Brick : IComponentData
+        {
+            public BrickKind kind;
+            public BrickPath path;
+            public int extraBalls;
+            public float despawnDelayAfterHit;
+            public float score;
+        }
+
+        public struct PathMotion : IComponentData
+        {
+            public float2 originXY;
+            public float amplitude;
+            public float speed;
+            public float phase;
+        }
+
+        public enum BrickKind : byte
+        {
+            Normal,
+            Gold,
+            Multiplier
+        }
+
+        public enum BrickPath : byte
+        {
+            None,
+            Infinity,
+            M,
+            Z
+        }
+    }
+}
---
+++ Assets/Project/Scripts/Systems/BrickMovementSystem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Project.Scripts.Authoring;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics.Systems;
+using Unity.Transforms;
+
+namespace Project.Scripts.Systems
+{
+    [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+    [UpdateBefore(typeof(PhysicsSimulationGroup))]
+    public partial struct BrickMovementSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnUpdate(ref SystemState s)
+        {
+            var timeElapsed = (float)SystemAPI.Time.ElapsedTime;
+
+            foreach (var (lt, brick, path, entity) in
+                     SystemAPI
+                         .Query<RefRW<LocalTransform>, RefRO<BrickAuthoring.Brick>, RefRO<BrickAuthoring.PathMotion>>()
+                         .WithEntityAccess())
+            {
+                var startPoint = path.ValueRO.originXY;
+                var amplitude = path.ValueRO.amplitude;
+                var speed = path.ValueRO.speed;
+                var phase = path.ValueRO.phase;
+
+                float2 newXY;
+                switch (brick.ValueRO.path)
+                {
+                    case BrickAuthoring.BrickPath.Infinity:
+                        newXY = startPoint + new float2(
+                            amplitude * math.sin(speed * timeElapsed + phase),
+                            (amplitude * 0.6f) * math.sin(2f * speed * timeElapsed + phase));
+                        break;
+
+                    case BrickAuthoring.BrickPath.M:
+                    {
+                        var m = math.sin(speed * timeElapsed + phase);
+                        newXY = startPoint + new float2(
+                            amplitude * math.sign(m) * math.pow(math.abs(m), 0.5f),
+                            (amplitude * 0.4f) * (1f - math.abs(m)));
+                    }
+                        break;
+
+                    case BrickAuthoring.BrickPath.Z:
+                    {
+                        var saw = 2f * (timeElapsed * speed - math.floor(0.5f + timeElapsed * speed));
+                        newXY = startPoint + new float2(
+                            amplitude * saw,
+                            amplitude * 0.2f * math.sin(2f * speed * timeElapsed + phase));
+                    }
+                        break;
+
+                    case BrickAuthoring.BrickPath.None:
+                        newXY = new float2(lt.ValueRO.Position.x, lt.ValueRO.Position.y);
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                var pos = lt.ValueRO.Position;
+                pos.x = newXY.x;
+                pos.y = newXY.y; 
+                lt.ValueRW.Position = pos;
+            }
+        }
+    }
+}
