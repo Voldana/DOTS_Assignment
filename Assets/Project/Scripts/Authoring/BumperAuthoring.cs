5b1ee17876a4cff3414db782c0bd6b7bdf2e89ee
Mohammad Darandeh <m.darande@gmail.com>
2025-08-22 15:59:33 -0400

    feat: implement bumper logic

---
+++ Assets/Project/Scripts/Authoring/BumperAuthoring.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Project.Scripts.Authoring
+{
+    public class BumperAuthoring : MonoBehaviour
+    {
+        [SerializeField] private float distance;
+        [SerializeField] private float speed;
+
+        class Baker : Baker<BumperAuthoring>
+        {
+            public override void Bake(BumperAuthoring authoring)
+            {
+                var e = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(e,
+                    new Bumper
+                    {
+                        distance = authoring.distance, speed = authoring.speed, phase = 0f,
+                        startY = authoring.transform.position.y
+                    });
+            }
+        }
+    }
+
+    public struct Bumper : IComponentData
+    {
+        public float distance, speed, phase, startY;
+    }
+}
---
+++ Assets/Project/Scripts/Systems/BumperMovementSystem.cs
@@ -0,0 +1,44 @@
+﻿using Project.Scripts.Authoring;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics.Systems;
+using Unity.Transforms;
+
+namespace Project.Scripts.Systems
+{
+    [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+    [UpdateBefore(typeof(PhysicsSimulationGroup))]
+    public partial struct BumperMovementSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var job = new BumperMoverJob
+            {
+                timeElapsed = (float)SystemAPI.Time.ElapsedTime
+            };
+            job.ScheduleParallel();
+        }
+    }
+
+    [BurstCompile]
+    public partial struct BumperMoverJob : IJobEntity
+    {
+        public float timeElapsed;
+
+        public void Execute(ref LocalTransform localTransform,
+            in Bumper bumper)
+        {
+            const float tau = 6.28318530718f;
+            var u = math.frac((bumper.speed * timeElapsed + bumper.phase) / tau);
+            var tri01 = 1f - math.abs(2f * u - 1f);
+
+            var y = bumper.startY + bumper.distance * tri01;
+
+            var p = localTransform.Position;
+            p.y = y;
+            localTransform.Position = p;
+        }
+    }
+}
