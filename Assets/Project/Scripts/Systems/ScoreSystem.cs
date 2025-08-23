776279c0c9452ad2012f6fdbd685517cd9986cf6
Mohammad Darandeh <m.darande@gmail.com>
2025-08-23 11:50:21 -0400

    feat: add score system

---
+++ Assets/Project/Scripts/Systems/ScoreSystem.cs
@@ -0,0 +1,22 @@
+﻿using Project.Scripts.Mono;
+using Unity.Burst;
+using Unity.Entities;
+
+namespace Project.Scripts.Systems
+{
+    [BurstCompile]
+    public partial struct ScoreSystem : ISystem
+    {
+        public void OnCreate(ref SystemState state)
+        {
+            var entity = state.EntityManager.CreateEntity();
+            state.EntityManager.AddComponentData(entity, new ScoreAccumulatorTag());
+            state.EntityManager.AddBuffer<ScoreEvent>(entity);
+        }
+
+    }
+
+    public struct ScoreAccumulatorTag : IComponentData
+    {
+    }
+}
